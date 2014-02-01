c4512bec7a5df33396359f894326b9f7064d42d8
Mak Tomilov <mak.tomilov@gmail.com>
2014-02-01 10:33:14 +0600

    Наброски

---
+++ tests/uni.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import wav_graph
+
+
+class TestWaveParse(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = wav_graph.WaveParse("test.wav")
+
+    def testReadWave(self):
+        pass
---
+++ wav_graph.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Library for parsing wave-files."""
+
+
+import wave
+
+
+class WaveParse(object):
+    """Объект предназначен для парсинга wave-файлов и
+    содержит информацию о спарсенном файле.
+    """
+
+    def __init__(self, filename):
+        """Напишу чего-нибудь позже
+
+        Arguments:
+        - `filename`: путь до файла
+        """
+        self._filename = filename
+
+
+class WaveGraph(object):
+    """Рисует различные графики, применяет фильтры.
+    """
+
+    def __init__(self, waveparse):
+        """Первоначально мы выполняем действия только
+        для отрисовки волны.
+
+        Arguments:
+        - `waveparse`: инстанс класса WaveParse
+        """
+        self._waveparse = waveparse
+
+
+def main():
+    """
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
